089c2b2bd731d2291ce46eacbf57935da61bdef6
aymendakir <aymendakr@yahoo.com>
2024-11-03 15:47:43 +0100

    category section

--- src/Components/Pages/Home/Category.tsx
+++ src/Components/Pages/Home/Category.tsx
@@ -4,6 +4,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useRef } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import accessories from "../../../assets/Images/Accesseoires.jpg";
+import food from "../../../assets/Images/food.jpg";
+import furniture from "../../../assets/Images/furniture.jpg";
+import bags from "../../../assets/Images/pags.jpg";
+import bath from "../../../assets/Images/soin.jpg";
 const Category = () => {
   let settings = {
     dots: false,
@@ -69,39 +74,64 @@ const Category = () => {
         }}
         className="grid grid-cols-4 justify-items-center gap-x-20 "
       >
-        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
+        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px] shadow-sm shadow-slate-300 mb-4">
+          <div className="image w-full h-[70%] bg-gray-200 rounded-t-[20px]">
+            <img
+              src={accessories}
+              alt="accessories"
+              title="accessories"
+              className="w-full h-full rounded-t-[20px]"
+            />
+          </div>
           <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
           <p className="text-black/50 text-sm ml-2">20 Products</p>
         </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
+        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px] shadow-sm shadow-slate-300">
+          <div className="image w-full h-[70%] bg-gray-200 rounded-t-[20px]">
+            <img
+              src={food}
+              alt="food"
+              title="food"
+              className="w-full h-full rounded-t-[20px]"
+            />
+          </div>
+          <p className="font-bold text-[20px] mt-2 ml-2">Food</p>
           <p className="text-black/50 text-sm ml-2">20 Products</p>
         </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
+        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px] shadow-sm shadow-slate-300">
+          <div className="image w-full h-[70%] bg-gray-200 rounded-t-[20px]">
+            <img
+              src={furniture}
+              alt="furniture"
+              title="furniture"
+              className="w-full h-full rounded-t-[20px]"
+            />
+          </div>
+          <p className="font-bold text-[20px] mt-2 ml-2">Furniture</p>
           <p className="text-black/50 text-sm ml-2">20 Products</p>
         </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
+        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px] shadow-sm shadow-slate-300">
+          <div className="image w-full h-[70%] bg-gray-200 rounded-t-[20px]">
+            <img
+              src={bags}
+              alt="bags"
+              title="bags"
+              className="w-full h-full rounded-t-[20px]"
+            />
+          </div>
+          <p className="font-bold text-[20px] mt-2 ml-2">Bags</p>
           <p className="text-black/50 text-sm ml-2">20 Products</p>
         </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
-          <p className="text-black/50 text-sm ml-2">20 Products</p>
-        </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
-          <p className="text-black/50 text-sm ml-2">20 Products</p>
-        </div>
-        <div className="h-[308px] !w-[300px] bg-gray-100/80 rounded-[20px]">
-          <div className="image w-full h-[70%] bg-black rounded-t-[20px]"></div>
-          <p className="font-bold text-[20px] mt-2 ml-2">Accessories</p>
+        <div className="h-[308px] !w-[300px]  bg-gray-100/80 rounded-[20px] shadow-sm shadow-slate-300 border border-gray-100">
+          <div className="image w-full h-[70%] bg-gray-200 rounded-t-[20px]">
+            <img
+              src={bath}
+              alt="bath"
+              title="bath"
+              className="w-full h-full rounded-t-[20px]"
+            />
+          </div>
+          <p className="font-bold text-[20px] mt-2 ml-2">Bath</p>
           <p className="text-black/50 text-sm ml-2">20 Products</p>
         </div>
       </Slider>
